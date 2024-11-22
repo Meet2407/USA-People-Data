580d5d48ffcafadeb3da1c804e8d6ce4cf0128c8
Meet2407 <meetkapadiya65@gmail.com>
2024-11-22 10:46:38 +0530

    Initial Commit

---
+++ USA People Data/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  USA People Data
+//
+//  Created by R90 on 22/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
